094f0f73da9cf180fb8952225ca9c3bb82262e85
webgovernor <webgovernor@gmail.com>
2013-11-09 13:04:03 -0500

    Restructuring for setup.py

---
+++ setup.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+from distutils.core import setup
+import os
+import sys
+import os
+
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
+sys.path.insert(0, os.path.dirname(__file__))
+from pykwiki import __version__
+
+setup(name='pykwiki',
+    version = __version__,
+    description = 'A simple pure-python wiki engine.',
+    long_description = '''
+A pure-python, light-weight, static wiki, blog, website engine designed for speed. 
+    ''',
+    author = 'Aaron Meier',
+    author_email = 'webgovernor@gmail.com',
+    packages = ['pykwiki','pykwiki.ext'],
+    package_dir={'pykwiki':'pykwiki'},
+    scripts=['pykwiki/scripts/pykwiki'],
+    package_data={'pykwiki':['data/*.zip']},
+    url = 'http://pykwiki.nullism.com',
+    license = 'MIT',
+    requires = ['markdown (>=2.0)', 'jinja2 (>=1.0)', 'pyyaml (>=3.0)'],
+    provides = ['pykwiki']
+)
+
